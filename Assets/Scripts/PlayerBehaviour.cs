1102655a893623e684d2d93c16db37c7012b3e42
Đặng Vũ Quang Minh <dvqminh98@gmail.com>
2022-07-27 17:11:50 +0700

    Minh Add Comment
    comment on PlayerBehaviour script

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,7 +7,7 @@ public class PlayerBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // Player Movement
     }
 
     // Update is called once per frame
